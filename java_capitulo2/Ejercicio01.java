c8082fb61f22d158d82eeacb37851da95ada74ee
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-10-05 09:03:45 +0200

    Java Capitulo 2

---
+++ java_capitulo2/Ejercicio01.java
@@ -0,0 +1,27 @@
+/* ******************************************************************************* */
+/*   File:Ejercicio01                                                              */
+/*                                                    ::::::::::       :;     ;:   */
+/*                                                   |;                :;    ;:    */
+/*                                                  |;                 :;   ;:     */
+/*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
+/*                                                |;                   :; ;:       */
+/*   Created: 2023/10/05 08:57 by pcamvaz        |::::::::::::   :+:   ;;;         */
+/*   Updated: 2023/10/05 09:02 by pcamvaz                                          */
+/*                                                                                 */
+/* ******************************************************************************* */
+package java_capitulo2;
+
+public class Ejercicio01 
+{
+    public static void main(String[] args)
+    {
+        int x, y;
+        x = 144;
+        y = 999;
+        System.out.println("El valor de x es: "+x+" y el valor de y es: "+y);
+        System.out.println("La suma de x+y = "+(x+y));
+        System.out.println("La resta de x-y = "+(x-y));
+        System.out.println("La multiplicacion de x*y = "+(x*y));
+        System.out.println("La division de x/y = "+(x/y));
+    }
+}
