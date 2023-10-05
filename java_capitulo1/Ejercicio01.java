64f236ff45a8b88d468fc24d423b30ca0fefe3f7
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-10-05 08:29:24 +0200

    Ejercicio01

---
+++ java_capitulo1/Ejercicio01.java
@@ -0,0 +1,20 @@
+/* ******************************************************************************* */
+/*   File:Ejercicio01                                                              */
+/*                                                    ::::::::::       :;     ;:   */
+/*                                                   |;                :;    ;:    */
+/*                                                  |;                 :;   ;:     */
+/*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
+/*                                                |;                   :; ;:       */
+/*   Created: 2019/10/16 18:53 by pcamvaz        |::::::::::::   :+:   ;;;         */
+/*   Updated: 2019/10/17 15:22 by pcamvaz                                          */
+/*                                                                                 */
+/* ******************************************************************************* */
+
+package java_capitulo1;
+
+public class Ejercicio01 
+{
+    public static void main(String[] args) {
+        System.out.println("Pablo Camino Vazquez");
+    }
+}
