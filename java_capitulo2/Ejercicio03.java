555fccfa5a72e43fdb98be95d115dafac753ca4c
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-10-05 09:14:38 +0200

    Ejercicio 3 Cap 2

---
+++ java_capitulo2/Ejercicio03.java
@@ -0,0 +1,24 @@
+/* ******************************************************************************* */
+/*   File:Ejercicio03                                                              */
+/*                                                    ::::::::::       :;     ;:   */
+/*                                                   |;                :;    ;:    */
+/*                                                  |;                 :;   ;:     */
+/*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
+/*                                                |;                   :; ;:       */
+/*   Created: 2023/10/05 09:10 by pcamvaz        |::::::::::::   :+:   ;;;         */
+/*   Updated: 2023/10/05 09:14 by pcamvaz                                          */
+/*                                                                                 */
+/* ******************************************************************************* */
+
+package java_capitulo2;
+
+public class Ejercicio03
+{
+    public static void main(String[] args)
+    {
+            String nombre = "Pablo Camino Vázquez";
+            String direccion =  "Pasillo de sierra blanca 14";
+            String tlf = "618381824";
+            System.out.print(nombre+"\n"+direccion+"\n"+tlf);
+    }
+}
