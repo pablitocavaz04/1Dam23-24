47f4f06be57c61ae3212080a6de6d12d26338021
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-10-05 10:32:08 +0200

    Ejercicio 08 Cap 2

---
+++ java_capitulo2/Ejercicio08.java
@@ -0,0 +1,28 @@
+/* ******************************************************************************* */
+/*   File:Ejercicio08.java                                                         */
+/*                                                    ::::::::::       :;     ;:   */
+/*                                                   |;                :;    ;:    */
+/*                                                  |;                 :;   ;:     */
+/*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
+/*                                                |;                   :; ;:       */
+/*   Created: 2023/10/05 10:26                   |::::::::::::   :+:   ;;;         */
+/*   Updated: 2023/10/05 10:31												       */
+/*                                                                                 */
+/* ******************************************************************************* */
+
+package java_capitulo2;
+
+public class Ejercicio08 
+{
+    public static void main(String[] args)
+    {
+        char l1 = 's';
+        char l2 = 'a';
+        char l3 = 'l';
+        char l4 = 'u';
+        char l5 = 'd';
+        String palabra = "" + l1 + l2 + l3 + l4 + l5;
+    
+        System.out.println(palabra);
+    }
+}
