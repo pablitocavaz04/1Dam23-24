3736ee66a98d6cc5ff4feffa7e9c6fcf7c760ca7
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-10-05 10:57:45 +0200

    Ejercicio04-5 Cap1

--- java_capitulo1/Ejercicio04.java
+++ java_capitulo1/Ejercicio04.java
@@ -1,25 +1,27 @@
 /* ******************************************************************************* */
-/*   File:Ejercicio03.java                                                         */
+/*   File:Ejercicio04.java                                                         */
 /*                                                    ::::::::::       :;     ;:   */
 /*                                                   |;                :;    ;:    */
 /*                                                  |;                 :;   ;:     */
 /*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
 /*                                                |;                   :; ;:       */
-/*   Created: 2023/10/05 10:32                   |::::::::::::   :+:   ;;;         */
-/*   Updated: 2023/10/05 10:32												       */
+/*   Created: 2023/10/05 10:55                   |::::::::::::   :+:   ;;;         */
+/*   Updated: 2023/10/05 10:55												       */
 /*                                                                                 */
 /* ******************************************************************************* */
+
 package java_capitulo1;
 
-public class Ejercicio03
+public class Ejercicio04
 {
     public static void main(String[] args)
     {
-        System.out.printf("%-10s %s\n", "computer", "ordenador");
-        System.out.printf("%-10s %s\n", "student", "alumno\\a");
-        System.out.printf("%-10s %s\n", "cat", "gato");
-        System.out.printf("%-10s %s\n", "penguin", "pingüino");
-        System.out.printf("%-10s %s\n", "machine", "máquina");
-        System.out.printf("%-10s %s\n", "nature", "naturaleza");
+        System.out.println("\n Lunes\tMartes\tMiérc.\tJueves.\tViernes");
+    System.out.println(" ======\t=======\t======\t=======\t=======");
+    System.out.println(" PROG\tPROG\tPROG\tPROG \tSIN");
+    System.out.println(" PROG\tPROG\tPROG\tPROG\tSIN");
+    System.out.println(" ED\tSIN\tSIN\tLM\tBDATO");
+    System.out.println(" FOL\tSIN\tSIN\tLM\tBDATO");
+    System.out.println(" FOL\tBDATO\tED\tBDATO\tED");
     }
 }
---
+++ java_capitulo1/Ejercicio05.java
@@ -0,0 +1,15 @@
+package java_capitulo1;
+
+public class Ejercicio05
+{
+    public static void main(String[] args)
+    {
+        System.out.println("\n Lunes\tMartes\tMiérc.\tJueves.\tViernes");
+        System.out.println(" ======\t=======\t======\t=======\t=======");
+        System.out.println(" PROG\tPROG\tPROG\tPROG \tSIN");
+        System.out.println(" PROG\tPROG\tPROG\tPROG\tSIN");
+        System.out.println(" ED\tSIN\tSIN\tLM\tBDATO");
+        System.out.println(" FOL\tSIN\tSIN\tLM\tBDATO");
+        System.out.println(" FOL\tBDATO\tED\tBDATO\tED");
+    }
+}
