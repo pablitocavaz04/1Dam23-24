720d42b9caad80f6301c36e34f61d76bea28692d
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-10-05 09:51:22 +0200

    Ejercicio 07 Cap2

---
+++ java_capitulo2/Ejercicio07.java
@@ -0,0 +1,25 @@
+/* ******************************************************************************* */
+/*   File:Ejercicio07                                                              */
+/*                                                    ::::::::::       :;     ;:   */
+/*                                                   |;                :;    ;:    */
+/*                                                  |;                 :;   ;:     */
+/*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
+/*                                                |;                   :; ;:       */
+/*   Created: 2023/10/05 09:49 by pcamvaz        |::::::::::::   :+:   ;;;         */
+/*   Updated: 2023/10/05 09:50 by pcamvaz                                          */
+/*                                                                                 */
+/* ******************************************************************************* */
+
+package java_capitulo2;
+
+public class Ejercicio07
+{
+    public static void main(String[] args)
+    {
+        char primeraLetra = 'P';
+        char ultimaLetra = 'C';
+        String palabra = "PabloCamino";
+    
+        System.out.println(primeraLetra + "" + ultimaLetra + palabra);
+    }
+}
