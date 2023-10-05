1fbbc68d788638118ba720dabf98985319632bfc
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-10-05 09:48:22 +0200

    Ejercicio 06 Cap2

--- java_capitulo2/Ejercicio06.java
+++ java_capitulo2/Ejercicio06.java
@@ -1,27 +1,27 @@
 /* ******************************************************************************* */
-/*   File:Ejercicio05                                                              */
+/*   File:Ejercicio06                                                              */
 /*                                                    ::::::::::       :;     ;:   */
 /*                                                   |;                :;    ;:    */
 /*                                                  |;                 :;   ;:     */
 /*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
 /*                                                |;                   :; ;:       */
-/*   Created: 2023/10/05 09:39 by pcamvaz        |::::::::::::   :+:   ;;;         */
-/*   Updated: 2023/10/05 09:41 by pcamvaz                                          */
+/*   Created: 2023/10/05 09:43 by pcamvaz        |::::::::::::   :+:   ;;;         */
+/*   Updated: 2023/10/05 09:47 by pcamvaz                                          */
 /*                                                                                 */
 /* ******************************************************************************* */
 
+
 package java_capitulo2;
 
-import java.util.Scanner;
-
-public class Ejercicio05
+public class Ejercicio06
 {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Introduzca la cantidad de pesetas que quieres convertir a euos: ");
-        int cantidad = sc.nextInt();
-        System.out.println("La cantidad de pesetas son : "+(cantidad/166)+" €");
-        
+        double baseImponible = 1724.75;
+
+        System.out.printf("Base imponible %8.2f\n", baseImponible);
+        System.out.printf("IVA            %8.2f\n", (baseImponible * 0.21));
+        System.out.printf("-----------------------\n");
+        System.out.printf("Total          %8.2f\n", (baseImponible * 1.21));
     }
 }
