d9cf5e4a90a3bdf8107f879002ce90a7d55b27d6
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-10-05 08:33:51 +0200

    Ejercicio02

--- java_capitulo1/Ejercicio02.java
+++ java_capitulo1/Ejercicio02.java
@@ -1,22 +1,20 @@
 /* ******************************************************************************* */
-/*   File:Ejercicio01                                                              */
+/*   File:Ejercicio02                                                              */
 /*                                                    ::::::::::       :;     ;:   */
 /*                                                   |;                :;    ;:    */
 /*                                                  |;                 :;   ;:     */
 /*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
 /*                                                |;                   :; ;:       */
-/*   Created: 2023/10/05 08:27 by pcamvaz        |::::::::::::   :+:   ;;;         */
-/*   Updated: 2023/10/05 08:29 by pcamvaz                                          */
+/*   Created: 2023/10/05 08:32 by pcamvaz        |::::::::::::   :+:   ;;;         */
+/*   Updated: 2023/10/05 08:33 by pcamvaz                                          */
 /*                                                                                 */
 /* ******************************************************************************* */
-
 package java_capitulo1;
 
-public class Ejercicio01 
-{
+public class Ejercicio02 {
     public static void main(String[] args) {
-        System.out.println("Pablo Camino Vazquez");
+        System.out.println("Pablo Camino Vázquez");
+        System.out.println("Pasillo de sierra blanca 14,1");
+        System.out.println("Tlf: 618381824");
     }
 }
-
-
