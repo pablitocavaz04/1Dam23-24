91e616c92c960ce801e53bfdfa72242814267eb1
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-10-05 09:41:35 +0200

    Ejercicio 05 Cap2

--- java_capitulo2/Ejercicio05.java
+++ java_capitulo2/Ejercicio05.java
@@ -1,27 +1,25 @@
 /* ******************************************************************************* */
-/*   File:Ejercicio04                                                             */
+/*   File:Ejercicio05                                                              */
 /*                                                    ::::::::::       :;     ;:   */
 /*                                                   |;                :;    ;:    */
 /*                                                  |;                 :;   ;:     */
 /*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
 /*                                                |;                   :; ;:       */
-/*   Created: 2023/10/05 09:15 by pcamvaz        |::::::::::::   :+:   ;;;         */
-/*   Updated: 2023/10/05 09:22 by pcamvaz                                          */
+/*   Created: 2023/10/05 09:39 by pcamvaz        |::::::::::::   :+:   ;;;         */
+/*   Updated: 2023/10/05 09:41 by pcamvaz                                          */
 /*                                                                                 */
 /* ******************************************************************************* */
-
 package java_capitulo2;
 
 import java.util.Scanner;
 
-public class Ejercicio04
-{
+public class Ejercicio05 {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Introduzca la cantidad de Euros que quieres convertir a pesetas: ");
+        System.out.print("Introduzca la cantidad de pesetas que quieres convertir a euos: ");
         int cantidad = sc.nextInt();
-        System.out.println("La cantidad de pesetas son : "+(cantidad*166));
+        System.out.println("La cantidad de pesetas son : "+(cantidad/166)+" €");
         
     }
 }
