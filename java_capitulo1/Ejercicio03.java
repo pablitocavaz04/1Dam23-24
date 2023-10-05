cfd46710e52a09bbeeb6771ae40a9e638680bb8c
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-10-05 10:54:38 +0200

    Ejercicio3 Cap1

---
+++ java_capitulo1/Ejercicio03.java
@@ -0,0 +1,23 @@
+/* ******************************************************************************* */
+/*   File:Ejercicio03.java                                                         */
+/*                                                    ::::::::::       :;     ;:   */
+/*                                                   |;                :;    ;:    */
+/*                                                  |;                 :;   ;:     */
+/*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
+/*                                                |;                   :; ;:       */
+/*   Created: 2023/10/05 10:32                   |::::::::::::   :+:   ;;;         */
+/*   Updated: 2023/10/05 10:32												       */
+/*                                                                                 */
+/* ******************************************************************************* */
+package java_capitulo1;
+
+public class Ejercicio03 {
+    public static void main(String[] args) {
+        System.out.printf("%-10s %s\n", "computer", "ordenador");
+        System.out.printf("%-10s %s\n", "student", "alumno\\a");
+        System.out.printf("%-10s %s\n", "cat", "gato");
+        System.out.printf("%-10s %s\n", "penguin", "pingüino");
+        System.out.printf("%-10s %s\n", "machine", "máquina");
+        System.out.printf("%-10s %s\n", "nature", "naturaleza");
+    }
+}
