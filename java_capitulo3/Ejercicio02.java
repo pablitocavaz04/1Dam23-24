bfd0c74d938dc59e5e692f03f0f14ab8eb521959
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-10-09 09:17:39 +0200

    Ejercicio02 capitulo 3

--- java_capitulo3/Ejercicio02.java
+++ java_capitulo3/Ejercicio02.java
@@ -6,7 +6,7 @@
 /*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
 /*                                                |;                   :; ;:       */
 /*   Created: 2023/10/09 08:58                   |::::::::::::   :+:   ;;;         */
-/*   Updated: 2023/10/09 08:58												       */
+/*   Updated: 2023/10/09 09.17												       */
 /*                                                                                 */
 /* ******************************************************************************* */
 package java_capitulo3;
@@ -17,12 +17,6 @@ public class Ejercicio02
 {
     public static void main(String[] args)
     {
-    //     System.out.print("Por favor, introduzca la cantidad de euros que quiere convertir: ");
-    // double euros = Double.parseDouble(System.console().readLine());
-
-    // int pesetas = (int) (euros * 166.386);
-    
-    // System.out.printf("%.2f euros son %d pesetas.", euros, pesetas);
         Scanner sc = new Scanner(System.in);
         System.out.print("Introduzca la cantidad de euros que quieres convertir: ");
         double euros = sc.nextDouble();
---
+++ java_capitulo3/Ejercicio03.java
@@ -0,0 +1,5 @@
+package java_capitulo3;
+
+public class Ejercicio03 {
+    
+}
