805d9a029cb952f84f3734dcf54e68142285514a
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-10-09 09:43:27 +0200

    Ejrcicio09 cap3

--- java_capitulo3/Ejercicio09.java
+++ java_capitulo3/Ejercicio09.java
@@ -1,25 +1,31 @@
 /* ******************************************************************************* */
-/*   File:Ejercicio08.java                                                         */
+/*   File:Ejercicio09.java                                                         */
 /*                                                    ::::::::::       :;     ;:   */
 /*                                                   |;                :;    ;:    */
 /*                                                  |;                 :;   ;:     */
 /*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
 /*                                                |;                   :; ;:       */
-/*   Created: 2023/10/09 09:34                  |::::::::::::   :+:   ;;;         */
-/*   Updated: 2023/10/09 09:38											       */
+/*   Created: 2023/10/09 09:39                   |::::::::::::   :+:   ;;;         */
+/*   Updated: 2023/10/09 09:41												       */
 /*                                                                                 */
 /* ******************************************************************************* */
 package java_capitulo3;
 
 import java.util.Scanner;
 
-public class Ejercicio08 
+public class Ejercicio09
 {
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Introduzca la cantidad de horas trabajadas: ");
-        int horas = sc.nextInt();
-        System.out.println("El salario que corresponde a "+horas+" trabajadas es = "+horas*12);
-    }
+        final double PI = 3.141592654;
+        System.out.println("Volumen de un cono");
+        System.out.println("------------------");
+        System.out.print("Por favor, introduzca la altura (cm): ");
+        double h = sc.nextDouble();
+        System.out.print("Introduzca el radio de la base (cm): ");
+        double r =sc.nextDouble();
+        double v = (1.0/3.0) * PI * r * r * h;
+        System.out.println("El volumen del cono es de " +  v + " cm³");
+    }    
 }
