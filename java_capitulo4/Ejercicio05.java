a50de95ddc5aae4e1ed7b0350dd8e0b7711be952
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-10-09 10:19:13 +0200

    Ejericio05 cap4

--- java_capitulo4/Ejercicio05.java
+++ java_capitulo4/Ejercicio05.java
@@ -18,5 +18,11 @@ public class Ejercicio05
     public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Este programa resuelve ecuaciones de primer grado del tipo ax + b = 0");
+        System.out.print("Introduzca el valor de a: ");
+        float a = sc.nextFloat();
+        System.out.print("Introduzca el valor de b: ");
+        float b = sc.nextFloat();
+        System.out.printf("x = %.2f",(b/a));
     }
 }
