2f26d92ec1ec2ce545bbe1a44a01f40f30e11664
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-10-09 09:32:04 +0200

    Ejercicio06 cap3

--- java_capitulo3/Ejercicio06.java
+++ java_capitulo3/Ejercicio06.java
@@ -6,15 +6,24 @@
 /*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
 /*                                                |;                   :; ;:       */
 /*   Created: 2023/10/09 09:29                   |::::::::::::   :+:   ;;;         */
-/*   Updated: 2023/10/09 09:29												       */
+/*   Updated: 2023/10/09 09:31											       */
 /*                                                                                 */
 /* ******************************************************************************* */
 package java_capitulo3;
+
+import java.util.Scanner;
 
 public class Ejercicio06
 {
     public static void main(String[] args)
     {
-    
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Área de un triangulo");
+        System.out.println("---------------------");
+        System.out.print("Por favor, introduzca la longitud de la base (cm): ");
+        double base = sc.nextDouble();
+        System.out.print("Introduzca la altura (cm): ");
+        double altura = sc.nextDouble();
+        System.out.println("El área del rectángulo es " + ((base * altura/2)) + " cm²");
     }
 }
