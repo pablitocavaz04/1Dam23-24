0e48f7c6b10709475a2857910f9495e0675ce481
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-10-17 13:26:58 +0200

    Ejercicio06 Cap5

---
+++ java_capitulo5/Ejercicio06.java
@@ -0,0 +1,26 @@
+/* ******************************************************************************* */
+/*   File:Ejercicio06.java                                                         */
+/*                                                    ::::::::::       :;     ;:   */
+/*                                                   |;                :;    ;:    */
+/*                                                  |;                 :;   ;:     */
+/*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
+/*                                                |;                   :; ;:       */
+/*   Created: 2023/10/17 13:25                   |::::::::::::   :+:   ;;;         */
+/*   Updated: 2023/10/17 13:26												       */
+/*                                                                                 */
+/* ******************************************************************************* */
+package java_capitulo5;
+
+public class Ejercicio06 
+{
+    public static void main(String[] args) 
+    {
+        int i;
+        i = 320;
+        System.out.print("Los numeros son = ");
+        do {
+            System.out.print(i+" ");
+            i-=20;
+        } while (i >= 160);
+    }
+}
