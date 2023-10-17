99490768225535ea3a844f56c610e686cfac5c72
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-10-17 12:59:57 +0200

    Ejercicio18-19 cap4

---
+++ java_capitulo4/Ejercicio18.java
@@ -0,0 +1,43 @@
+/* ******************************************************************************* */
+/*   File:Ejercicio18.java                                                         */
+/*                                                    ::::::::::       :;     ;:   */
+/*                                                   |;                :;    ;:    */
+/*                                                  |;                 :;   ;:     */
+/*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
+/*                                                |;                   :; ;:       */
+/*   Created: 2023/10/17 12:58                   |::::::::::::   :+:   ;;;         */
+/*   Updated: 2023/10/17 12:58												       */
+/*                                                                                 */
+/* ******************************************************************************* */
+package java_capitulo4;
+
+public class Ejercicio18 {
+    public static void main(String[] args) {
+        int n, primera = 0;
+    
+    System.out.print("Por favor, introduzca un número entero positivo (5 cifras como máximo): ");
+    n = Integer.parseInt(System.console().readLine());
+    
+    if ( n < 10 ) {
+      primera = n;
+    }
+    
+    if (( n >= 10 ) && ( n < 100 )) {
+      primera = n / 10;
+    }
+    
+    if (( n >= 100 ) && ( n < 1000 )) {
+      primera = n / 100;
+    }
+    
+    if (( n >= 1000 ) && ( n < 10000 )) {
+      primera = n / 1000;
+    }
+    
+    if ( n >= 10000 ) {
+      primera = n / 10000;
+    }
+    
+    System.out.println("La primera cifra del número introducido es el " + primera + ".");
+    }
+}
