f2ca56503a68fe07ab6c4e5e34898723c48cd42c
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-10-24 13:35:24 +0200

    Ejercicio28 cap5

---
+++ java_capitulo5/Ejercicio28.java
@@ -0,0 +1,28 @@
+/* ******************************************************************************* */
+/*   File:Ejercicio28.java                                                         */
+/*                                                    ::::::::::       :;     ;:   */
+/*                                                   |;                :;    ;:    */
+/*                                                  |;                 :;   ;:     */
+/*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
+/*                                                |;                   :; ;:       */
+/*   Created: 2023/10/24 13:31                   |::::::::::::   :+:   ;;;         */
+/*   Updated: 2023/10/24 13:35												       */
+/*                                                                                 */
+/* ******************************************************************************* */
+package java_capitulo5;
+
+import java.util.Scanner;
+
+public class Ejercicio28 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Por favor, introduzca un numero entero: ");
+        int numero = Integer.parseInt(sc.nextLine());
+        int total = 1;
+        for (int i = 1; i <= numero; i++)
+            total*=i;
+        System.out.println(numero+"! = "+total);
+    }
+}
