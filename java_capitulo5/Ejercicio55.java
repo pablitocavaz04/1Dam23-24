8db0014d3bfd81bcf69034a5309fd617080a4a1a
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-11-02 08:30:33 +0100

    Ejercicio55 Cap5

--- java_capitulo5/Ejercicio55.java
+++ java_capitulo5/Ejercicio55.java
@@ -1,36 +1,37 @@
 /* ******************************************************************************* */
-/*   File:Ejercicio52.java                                                         */
+/*   File:Ejercicio55.java                                                         */
 /*                                                    ::::::::::       :;     ;:   */
 /*                                                   |;                :;    ;:    */
 /*                                                  |;                 :;   ;:     */
 /*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
 /*                                                |;                   :; ;:       */
-/*   Created: 2023/10/31 00:22                   |::::::::::::   :+:   ;;;         */
-/*   Updated: 2023/10/31 00:25												       */
+/*   Created: 2023/11/02 08:29                   |::::::::::::   :+:   ;;;         */
+/*   Updated: 2023/11/02 08:29												       */
 /*                                                                                 */
 /* ******************************************************************************* */
-
 package java_capitulo5;
 
 import java.util.Scanner;
 
-public class Ejercicio52 {
-     public static void main(String[] args) {
+public class Ejercicio55 {
+
+  public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     System.out.print("Introduzca un número: ");
     long numeroIntroducido = Long.parseLong(s.nextLine());
-    
+
     long copia = numeroIntroducido;
     int longitud = 0;
-    
+
     while (copia > 0) {
       copia /= 10;
       longitud++;
     }
-    
-    int primero =  (int)(numeroIntroducido / Math.pow(10, longitud - 1));
-    long aux = (long)(primero * Math.pow(10, longitud - 1));
-    long resultado = (numeroIntroducido - aux) * 10 + primero;
+
+    int ultimo = (int) (numeroIntroducido % 10);
+    long aux = numeroIntroducido / 10;
+    long resultado = ultimo * (long) Math.pow(10, longitud - 1) + aux;
     System.out.println("El número resultado es " + resultado + ".");
   }
+
 }
