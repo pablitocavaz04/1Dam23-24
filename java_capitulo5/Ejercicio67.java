d0e0ac97f27aa85f58b2961f6b16e2de6362491b
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-11-06 08:57:55 +0100

    Ejercicio67 Cap5

--- java_capitulo5/Ejercicio67.java
+++ java_capitulo5/Ejercicio67.java
@@ -6,7 +6,7 @@
 /*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
 /*                                                |;                   :; ;:       */
 /*   Created: 2023/11/02 11:09                   |::::::::::::   :+:   ;;;         */
-/*   Updated: 2023/11/02 11:09												       */
+/*   Updated: 2023/11/06 08:57												       */
 /*                                                                                 */
 /* ******************************************************************************* */
 package java_capitulo5;
@@ -22,11 +22,14 @@ public class Ejercicio67
         int numEscalones = Integer.parseInt(sc.nextLine());
         System.out.print("Introduzca la altura de cada escalón: ");
         int altEscalon = Integer.parseInt(sc.nextLine());
+        int anchura = 4;
         for (int i = 1; i <= numEscalones*altEscalon; i++)
         {
-            for (int j = 1; j <= ; j++) {
-                
-            }
+            for (int j = 1; j <= anchura; j++)
+                System.out.print("*");
+        if (i % altEscalon == 0)
+            anchura +=4;
+        System.out.println();
         }
     }
 }
