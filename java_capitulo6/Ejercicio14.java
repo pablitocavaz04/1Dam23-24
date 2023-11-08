528700fdaeae8898a7d56280f918412c5f5b9e9f
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-11-08 11:10:56 +0100

    Ejercicio14 Cap6

---
+++ java_capitulo6/Ejercicio14.java
@@ -0,0 +1,52 @@
+/* ******************************************************************************* */
+/*   File:Ejercicio14.java                                                         */
+/*                                                    ::::::::::       :;     ;:   */
+/*                                                   |;                :;    ;:    */
+/*                                                  |;                 :;   ;:     */
+/*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
+/*                                                |;                   :; ;:       */
+/*   Created: 2023/11/08 11:06                   |::::::::::::   :+:   ;;;         */
+/*   Updated: 2023/11/08 11:15												       */
+/*                                                                                 */
+/* ******************************************************************************* */
+package java_capitulo6;
+
+public class Ejercicio14
+{
+    public static void main(String[] args) {
+    
+        int oportunidades = 5;
+        int numeroPensado;
+        int minimo = 0;
+        int maximo = 100;
+        boolean acertado = false;
+        int mayorMenorOIgual;
+            
+        System.out.println("Piensa un número del 0 al 100. Intentaré adivinarlo en 5 intentos.");
+        System.out.println("Pulsa la tecla INTRO cuando estés preparado.");
+        System.console().readLine();
+    
+        do {
+          numeroPensado = (int)(Math.random() * (maximo - minimo) + minimo);
+          System.out.println("¿Es el " + numeroPensado + "?");
+          System.out.print("El número que estás pensando es 1) mayor 2) menor 3) el mismo: ");
+          mayorMenorOIgual = Integer.parseInt(System.console().readLine());
+          oportunidades--;
+    
+          if ( (mayorMenorOIgual == 1) && (oportunidades > 0) )
+            minimo = numeroPensado + 1;
+          
+          if ( (mayorMenorOIgual == 2) && (oportunidades > 0) )
+            maximo = numeroPensado - 1;
+          
+          if (mayorMenorOIgual == 3) {
+            acertado = true;
+            System.out.println("¡Bien! ¡he acertado!");
+          }
+        } while(!acertado && (oportunidades > 0));
+        
+        if (!acertado) {
+          System.out.println("Vaya, no he conseguido acertar el número.");
+        }
+      }
+}
