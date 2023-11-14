b4a5d0be0dea087b1c38a0070b876453ea69e0d9
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-11-14 12:22:58 +0100

    Ejercicio27 Cap6

--- java_capitulo6/Ejercicio27.java
+++ java_capitulo6/Ejercicio27.java
@@ -6,7 +6,7 @@
 /*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
 /*                                                |;                   :; ;:       */
 /*   Created: 2023/11/09 11:05                   |::::::::::::   :+:   ;;;         */
-/*   Updated: 2023/11/09 11:05												       */
+/*   Updated: 2023/11/14 12:22												       */
 /*                                                                                 */
 /* ******************************************************************************* */
 package java_capitulo6;
@@ -18,7 +18,7 @@ public class Ejercicio27
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Turno del jugador (introduzca piedra, papel o tijera): ");
+        System.out.print("Turno del jugador (introduzca piedra, papel o tijera): ");
         String jugador = sc.nextLine();
         String ordenador = "";
         switch ((int)(Math.random()*3)+1)
@@ -38,6 +38,17 @@ public class Ejercicio27
         System.out.println("Turno del ordenador : "+ordenador);
         if (jugador == ordenador)
             System.out.println("empate");
-        else if(jugador = 'piedra' && )
+        else if(jugador == "piedra" && ordenador == "tijera")
+            System.out.println("Gana el jugador");
+        else if (jugador == "tijera" && ordenador == "piedra")
+            System.out.println("Gana el ordenador");
+        else if (jugador == "papel" && ordenador == "piedra")
+            System.out.println("Gana el jugador");
+        else if (jugador == "piedra" && ordenador == "papel")
+            System.out.println("Gana el ordenador");
+        else if (jugador == "tijera" && ordenador == "papel")
+            System.out.println("Gana el jugador");
+        else if (jugador == "papel" && ordenador == "tijera")
+            System.out.println("Gana el ordenador");
     }
 }
