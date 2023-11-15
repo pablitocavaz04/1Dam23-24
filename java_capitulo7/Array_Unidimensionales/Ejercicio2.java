f3115da6703ee88aa50d805c10cb36c7cf552363
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-11-15 12:14:00 +0100

    Ejercicio02 Cap7Uni

---
+++ java_capitulo7/Array_Unidimensionales/Ejercicio2.java
@@ -0,0 +1,30 @@
+/* ******************************************************************************* */
+/*   File:Ejercicio2.java                                                         */
+/*                                                    ::::::::::       :;     ;:   */
+/*                                                   |;                :;    ;:    */
+/*                                                  |;                 :;   ;:     */
+/*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
+/*                                                |;                   :; ;:       */
+/*   Created: 2023/11/15 12:06                   |::::::::::::   :+:   ;;;         */
+/*   Updated: 2023/11/15 12:13												       */
+/*                                                                                 */
+/* ******************************************************************************* */
+package java_capitulo7.Array_Unidimensionales;
+
+public class Ejercicio2
+{
+    public static void main(String[] args)
+    {
+        String [] simbolo = new String[10];
+
+        simbolo [0] = "a";
+        simbolo [1] = "x";
+        simbolo [4] = "@";
+        simbolo [6] = " ";
+        simbolo [7] = "+";
+        simbolo [8] = "Q";
+        for (int i = 0; i < simbolo.length; i++)
+        System.out.println("simbolo[" + i + "]: " + simbolo[i]);
+         
+    }
+}
