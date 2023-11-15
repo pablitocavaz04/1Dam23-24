7858c58430b9c21cbf20bc19685f276c8fe839c1
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-11-15 12:39:34 +0100

    Ejercicio03 Cap7Uni

--- java_capitulo7/Array_Unidimensionales/Ejercicio3.java
+++ java_capitulo7/Array_Unidimensionales/Ejercicio3.java
@@ -6,13 +6,25 @@
 /*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
 /*                                                |;                   :; ;:       */
 /*   Created: 2023/11/15 12:13                   |::::::::::::   :+:   ;;;         */
-/*   Updated: 2023/11/15 12:13												       */
+/*   Updated: 2023/11/15 12:39												       */
 /*                                                                                 */
 /* ******************************************************************************* */
 package java_capitulo7.Array_Unidimensionales;
 
-public class Ejercicio3 {
-    public static void main(String[] args) {
-        
+import java.util.Scanner;
+
+public class Ejercicio3
+{
+    public static void main(String[] args)
+    {
+       Scanner sc = new Scanner(System.in);
+       int []  array1 = new int[10];
+       for (int i = 0; i < 10; i++)
+       {
+            System.out.print("Introduza el numero "+i+" :");
+            array1[i] = Integer.parseInt(sc.nextLine());
+       }
+       for (int i = 9; i >= 0 ; i--)
+            System.out.printf("array1[%2d] : %d\n",i,array1[i]);
     }
 }
