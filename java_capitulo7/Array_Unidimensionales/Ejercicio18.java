b8bad4c0d8a6b9e72fd78e058e72ed331b855ca4
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-11-23 09:30:33 +0100

    Ejercicio18 Cap7

---
+++ java_capitulo7/Array_Unidimensionales/Ejercicio18.java
@@ -0,0 +1,86 @@
+/* ******************************************************************************* */
+/*   File:Ejercicio18.java                                                         */
+/*                                                    ::::::::::       :;     ;:   */
+/*                                                   |;                :;    ;:    */
+/*                                                  |;                 :;   ;:     */
+/*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
+/*                                                |;                   :; ;:       */
+/*   Created: 2023/11/23 09:30                   |::::::::::::   :+:   ;;;         */
+/*   Updated: 2023/11/23 09:30												       */
+/*                                                                                 */
+/* ******************************************************************************* */
+package java_capitulo7.Array_Unidimensionales;
+
+public class Ejercicio18 {
+    public static void main(String[] args) {
+        int[] a = new int[10];
+    int[] menores = new int[10];
+    int[] mayores = new int[10];
+    int cuentaMenores = 0;
+    int cuentaMayores = 0;
+    
+    // Genera el array original y clasifica los números
+    for (int i = 0; i < 10; i++) {
+      a[i] = (int) (Math.random() * 201);
+      
+      if (a[i] <= 100) {
+        menores[cuentaMenores++] = a[i];
+      } else {
+        mayores[cuentaMayores++] = a[i];
+      }
+    }
+
+    // Muestra el array original
+    System.out.println("\nArray original:");
+    System.out.println("┌────────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐");
+    System.out.print("│ Índice ");
+    for (int i = 0; i < 10; i++) {
+      System.out.printf("│%4d ", i);
+    }
+    System.out.println("│");
+    System.out.println("├────────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤");
+    System.out.print("│ Valor  ");
+    for (int i = 0; i < 10; i++) {
+      System.out.printf("│%4d ", a[i]);
+    }
+    System.out.println("│");
+    System.out.println("└────────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘");
+
+    // Genera el array resultado
+    int[] resultado = new int[10];
+    int menoresColocados = 0;
+    int mayoresColocados = 0;
+    
+    int j = 0;
+    do {
+      
+      if (menoresColocados < cuentaMenores) {
+        resultado[j++] = menores[menoresColocados++];
+      }
+      
+      if (mayoresColocados < cuentaMayores) {
+        resultado[j++] = mayores[mayoresColocados++];
+      }
+      
+    } while (j < 10);
+
+
+    // Muestra el resultado.
+    System.out.println("\nArray resultado:");
+    System.out.println("┌────────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐");
+    System.out.print("│ Índice ");
+    for (int i = 0; i < 10; i++) {
+      System.out.printf("│%4d ", i);
+    }
+    System.out.println("│");
+    System.out.println("├────────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤");
+    System.out.print("│ Valor  ");
+    for (int i = 0; i < 10; i++) {
+      System.out.printf("│%4d ", resultado[i]);
+    }
+    System.out.println("│");
+    System.out.println("└────────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘");
+  
+        
+    }
+}
