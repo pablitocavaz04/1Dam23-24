45fa508acbc48193de7ded40e6c3fa67bddfa489
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-11-23 09:38:42 +0100

    Ejercicio01 Bidiminesionales Cap7

--- java_capitulo7/Array_Bidimensionales/Ejercicio1.java
+++ java_capitulo7/Array_Bidimensionales/Ejercicio1.java
@@ -1,5 +1,45 @@
+/* ******************************************************************************* */
+/*   File:Ejercicio1.java                                                         */
+/*                                                    ::::::::::       :;     ;:   */
+/*                                                   |;                :;    ;:    */
+/*                                                  |;                 :;   ;:     */
+/*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
+/*                                                |;                   :; ;:       */
+/*   Created: 2023/11/23 09:31                   |::::::::::::   :+:   ;;;         */
+/*   Updated: 2023/11/23 09:38												       */
+/*                                                                                 */
+/* ******************************************************************************* */
 package java_capitulo7.Array_Bidimensionales;
 
-public class Ejercicio1 {
-    
+public class Ejercicio1
+{
+    public static void main(String[] args)throws InterruptedException     {
+        int [][] num = new int[3][6];
+        num[0][0] = 0;
+        num[0][1] = 30;
+        num[0][2] = 2;
+        num[0][5] = 7;
+        num[1][0] = 75;
+        num[1][4] = 0;
+        num[2][2] = -2;
+        num[2][3] = 9;
+        num[2][5] = 11;
+
+        int fila;
+        int columna;
+
+    System.out.print("       ");
+    for(columna = 0; columna < 6; columna++) {
+      System.out.print("   Columna " + columna);
+    }
+    for(fila = 0; fila < 3; fila++) {
+
+      System.out.print("\nFila " + fila);
+  
+      for(columna = 0; columna < 6; columna++) {
+        System.out.printf("%9d   ", num[fila][columna]);
+        Thread.sleep(500); // retardo de medio segundo
+      }
+    }
+    }
 }
