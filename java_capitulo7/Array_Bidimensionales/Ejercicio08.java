e9333c284d026c6897d0086cb502955f292d9c1a
pablitocavaz04 <pablocavaz2004@gmail.com>
2023-12-12 11:41:45 +0100

    Ejercicio08 Cap7Bi

--- java_capitulo7/Array_Bidimensionales/Ejercicio08.java
+++ java_capitulo7/Array_Bidimensionales/Ejercicio08.java
@@ -10,11 +10,30 @@
 /*                                                                                 */
 /* ******************************************************************************* */
 package java_capitulo7.Array_Bidimensionales;
+import java.util.Scanner;
+public class Ejercicio08{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
 
-public class Ejercicio08
-{
-    public static void main(String[] args)
-    {
-        
-    }
-}
+        System.out.print("\nIntroduzca la posición del alfil, por ejemplo d5: ");
+        String posicionAlfil = System.console().readLine();
+        int columnaAlfil = (int)(posicionAlfil.charAt(0)) - 96;
+        int filaAlfil = (int)(posicionAlfil.charAt(1)) - 48;
+    
+        System.out.println("El alfil puede moverse a las siguientes posiciones:");
+    
+        for(int fila = 8; fila >= 1; fila--) {
+
+            for(int columna = 1; columna <= 8; columna++) {
+
+                if ((Math.abs(filaAlfil - fila) == Math.abs(columnaAlfil - columna))
+                    && (! ((filaAlfil == fila) && (columnaAlfil == columna)))) {
+                System.out.print((char)(columna + 96) + "" + fila + " ");
+                
+                }
+            }
+        }
+    
+        sc.close();
+        }
+	}
