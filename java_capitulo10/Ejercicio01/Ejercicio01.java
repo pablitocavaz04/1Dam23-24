2d1c0fe592249c330931e0aca79f4df1fcbe780b
pablitocavaz04 <pablocavaz2004@gmail.com>
2024-01-25 08:28:53 +0100

    Ejercicio01 Cap10

--- java_capitulo10/Ejercicio01/Ejercicio01.java
+++ java_capitulo10/Ejercicio01/Ejercicio01.java
@@ -1,5 +1,33 @@
+/* ******************************************************************************* */
+/*   File:Ejercicio01.java                                                         */
+/*                                                    ::::::::::       :;     ;:   */
+/*                                                   |;                :;    ;:    */
+/*                                                  |;                 :;   ;:     */
+/*   By: Pablo Camino Vázquez                      |;                  :;  ;:      */
+/*                                                |;                   :; ;:       */
+/*   Created: 2024/01/25 08:20                   |::::::::::::   :+:   ;;;         */
+/*   Updated: 2024/01/25 08:20												       */
+/*                                                                                 */
+/* ******************************************************************************* */
 package java_capitulo10.Ejercicio01;
 
-public class Ejercicio01 {
-    
+import java.util.ArrayList;
+
+public class Ejercicio01
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Pablo");
+        names.add("Florentino");
+        names.add("Vinicius");
+        names.add("Lamine");
+        names.add("Tragabolas");
+        names.add("Kinki");
+        
+        for (int i = 0; i < names.size(); i++)
+        {
+            System.out.println("El nombre "+(i+1)+" es el nombre: "+names.get(i));
+        }
+    }
 }
